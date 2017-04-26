8cd5517cfabf0bd7e5dc1d6b4ea28b06790a1eea
Sumeet Minhas <sumeetm273@gmail.com>
2017-04-26 22:05:44 +0000

    registeration page errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
-  def show
+  def new
     @user = User.new
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/register'
+      render :new
     end
   end
 
